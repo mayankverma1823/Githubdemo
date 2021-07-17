9b253b72ac424fb89458f7fba0760f7726aa21bf
mayankverma1823@gmail.com <mayankverma1823@gmail.com>
2021-07-17 17:35:47 +0530

    changes to gitdemo

--- src/example.java
+++ src/example.java
@@ -3,7 +3,14 @@
 public class example {
 
 	public static void main(String[] args) {
-		System.out.println("sayhello");// TODO Auto-generated method s
+		System.out.println("sayhello");
+		int i=8;
+		int k=9;
+		int l = i+k;
+		
+
+System.out.println(l);
+System.out.println("demo was good");
 	}
 	
 
